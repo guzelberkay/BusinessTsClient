ebe9d6d03de8bb417e7f13e39f93c83efb399155
candenizgumus <denizgumus1996@hotmail.com>
2024-09-12 16:43:10 +0300

    ProductPage.tsx added to test datas.

--- src/store/feature/index.tsx
+++ src/store/feature/index.tsx
@@ -1,7 +1,9 @@
 import authSlice from "./authSlice";
 import languageSlice from "./languageSlice";
+import stockSlice from "./stockSlice.tsx";
 
 export {
     authSlice,
-    languageSlice
+    languageSlice,
+    stockSlice
 };
