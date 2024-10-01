39ff17aaf39751fb3249b07fa5106ea39d23dd57
guzelberkay <guzelberkay@outlook.com>
2024-10-01 17:25:09 +0300

    Yerel değişiklikler kaydedildi

--- src/config/EndPoints.tsx
+++ src/config/EndPoints.tsx
@@ -1,7 +1,25 @@
-import {subscriptionSlice} from "../store/feature";
+import { subscriptionSlice } from "../store/feature";
 
 // To hold the API endpoints for different services
 const endpoints = {
+  auth: "/auth", // Example: Endpoint for the authentication service
+  product_category: "/stock/product-category",
+  order: "/stock/order",
+  product: "/stock/product",
+  stock_movement: "/stock/stock-movement",
+  supplier: "/stock/supplier",
+  ware_house: "/stock/ware-house",
+  customerStock: "/stock/customer",
+  // CRM Endpoints
+  customer: "/crm/customer",
+  marketing_campaign: "/crm/marketing-campaign",
+  opportunity: "/crm/opportunity",
+  sales_activity:"/crm/sales-activity",
+  ticket:"/crm/ticket",
+  user: "/user",
+  role:"/role",
+  employee:"/employee",
+  notifications: "/notifications",
     auth: "/auth", // Example: Endpoint for the authentication service
     product_category: "/stock/product-category",
     order: "/stock/order",
@@ -9,15 +27,14 @@ const endpoints = {
     stock_movement: "/stock/stock-movement",
     supplier: "/stock/supplier",
     ware_house: "/stock/ware-house",
-    customerStock: "/stock/customer",
     // CRM Endpoints
     customer: "/crm/customer",
     marketing_campaign: "/crm/marketing-campaign",
     opportunity: "/crm/opportunity",
-    sales_activity: "/crm/sales-activity",
-    ticket: "/crm/ticket",
-    user: "/usermanagement/user",
-    role:"/usermanagement/role",
+    sales_activity:"/crm/sales-activity",
+    ticket:"/crm/ticket",
+    user: "/user",
+    role: "/role",
     employee: "/employee",
     notifications: "/notifications",
     // FAM Endpoints
@@ -28,9 +45,9 @@ const endpoints = {
     income: "/finance/income",
     invoice: "/finance/invoice",
     tax: "/finance/tax",
-    // SUB Endpoints
-    subscription: "/subscription",
-    plan: "/plan",
+
+  subscription: "/subscription",
+  plan: "/plan",
 };
 
 export default endpoints;
